55db7eacce31295b328793687ecd8156f1140878
Christopher P. Anderson <48180628+topherinternational@users.noreply.github.com>
2023-12-07 21:36:26 +0100

    pylint 2.17.7 (no changes)

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from pathlib import Path
 
 from setuptools import setup, find_packages
 
-requirements = ["pylint==2.14.5"]
+requirements = ["pylint==2.17.7"]
 
 readme_path = Path(__file__).resolve().parent / "README.rst"
 with open(readme_path, encoding="utf-8") as readme_file:
