c8ad9969ca399f12f63c1f6899ff88c9b065675c
Christopher P. Anderson <48180628+topherinternational@users.noreply.github.com>
2023-12-07 21:55:12 +0100

    Python version, version, looser pylint dependency

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from pathlib import Path
 
 from setuptools import setup, find_packages
 
-requirements = ["pylint==2.14.0"]
+requirements = ["pylint>=2.14.0,<4.0.0"]
 
 readme_path = Path(__file__).resolve().parent / "README.rst"
 with open(readme_path, encoding="utf-8") as readme_file:
@@ -15,7 +15,7 @@ setup(
     description="A Pylint plugin to lint Apache Airflow code.",
     long_description=long_description,
     long_description_content_type="text/x-rst",
-    version="0.1.1-alpha.1",
+    version="0.2.1",
     packages=find_packages(where="src"),
     package_dir={"": "src"},
     install_requires=requirements,
@@ -26,7 +26,6 @@ setup(
         "License :: OSI Approved :: MIT License",
         "Natural Language :: English",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
     ],
