fce699fbc7a8b8c4c6d417ac02f9e3230fa634b1
Christopher P. Anderson <48180628+topherinternational@users.noreply.github.com>
2023-12-17 00:00:05 +0100

    more refactor

--- src/pylint_airflow/checkers/dag.py
+++ src/pylint_airflow/checkers/dag.py
@@ -56,14 +56,27 @@ def dag_call_node_from_joined_string(
     # TODO: follow name chains
 
 
-def is_dag_by_name_or_attribute(func: astroid.NodeNG) -> bool:
+def dag_call_node_from_argument_value(
+    argument_value: astroid.NodeNG, call_node: astroid.Call
+) -> Optional[DagCallNode]:
+    """Detects argument string from Const, Name or JoinedStr (f-string), or None if no match"""
+    if isinstance(argument_value, astroid.Const):
+        return dag_call_node_from_const(argument_value, call_node)
+    if isinstance(argument_value, astroid.Name):
+        return dag_call_node_from_name(argument_value, call_node)
+    if isinstance(argument_value, astroid.JoinedStr):
+        return dag_call_node_from_joined_string(argument_value, call_node)
+    return None
+
+
+def is_dag_node_by_name_or_attribute(func: astroid.NodeNG) -> bool:
     """Check for either 'DAG(dag_id="mydag")' or e.g. 'models.DAG(dag_id="mydag")'"""
     return (isinstance(func, astroid.Name) and func.name == "DAG") or (
         isinstance(func, astroid.Attribute) and func.attrname == "DAG"
     )
 
 
-def is_subtype_of_dag(function_node: Optional[astroid.ClassDef]) -> bool:
+def is_inferred_value_subtype_of_dag(function_node: Optional[astroid.ClassDef]) -> bool:
     """Checks class type against DAG class types"""
     return function_node and (
         function_node.is_subtype_of("airflow.models.DAG")
@@ -117,7 +130,6 @@ class DagChecker(checkers.BaseChecker):
 
     @staticmethod
     def _find_dag_in_call_node(call_node: astroid.Call) -> Optional[DagCallNode]:
-        # pylint: disable=too-many-nested-blocks,too-many-return-statements
         """
         Find DAG in a call_node. Returns None if no DAG is found.
         :param call_node:
@@ -126,34 +138,22 @@ class DagChecker(checkers.BaseChecker):
 
         func = call_node.func
 
-        if not is_dag_by_name_or_attribute(func):
+        if not is_dag_node_by_name_or_attribute(func):
             return None
 
         function_node = safe_infer(func)
-        if is_subtype_of_dag(function_node):
-            # Check for "dag_id" as keyword arg
-            if call_node.keywords:
-                for keyword in call_node.keywords:
-                    if keyword.arg == "dag_id":
-                        kw_val = keyword.value
-                        if isinstance(kw_val, astroid.Const):
-                            return dag_call_node_from_const(kw_val, call_node)
-                        if isinstance(kw_val, astroid.Name):
-                            return dag_call_node_from_name(kw_val, call_node)
-                        if isinstance(kw_val, astroid.JoinedStr):
-                            return dag_call_node_from_joined_string(kw_val, call_node)
-                        return None
+        if not is_inferred_value_subtype_of_dag(function_node):
+            return None
 
-            # Check for dag_id as positional arg if we didn't find the dag_id keyword arg
-            if call_node.args:
-                first_positional_arg = call_node.args[0]
-                if isinstance(first_positional_arg, astroid.Const):
-                    return dag_call_node_from_const(first_positional_arg, call_node)
-                if isinstance(first_positional_arg, astroid.Name):
-                    return dag_call_node_from_name(first_positional_arg, call_node)
-                if isinstance(first_positional_arg, astroid.JoinedStr):
-                    return dag_call_node_from_joined_string(first_positional_arg, call_node)
-                return None
+        # Check for "dag_id" as keyword arg
+        if call_node.keywords:
+            for keyword in call_node.keywords:
+                if keyword.arg == "dag_id":
+                    return dag_call_node_from_argument_value(keyword.value, call_node)
+
+        # Check for dag_id as 0-th positional arg if we didn't find the dag_id keyword arg
+        if call_node.args:
+            return dag_call_node_from_argument_value(call_node.args[0], call_node)
 
         # if we found neither a keyword arg or a positional arg
         return None
