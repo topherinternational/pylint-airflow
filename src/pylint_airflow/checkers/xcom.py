96b1ad4b41856809c73a3a5edaa917e1eba5aa4c
Christopher P. Anderson <48180628+topherinternational@users.noreply.github.com>
2023-12-21 20:43:05 +0100

    the other function pulled out

--- src/pylint_airflow/checkers/xcom.py
+++ src/pylint_airflow/checkers/xcom.py
@@ -22,12 +22,13 @@ XCOM_CHECKER_MSGS = {
 def get_task_ids_to_python_callable_specs(
     node: astroid.Module,
 ) -> Dict[str, Tuple[astroid.Call, str]]:
+    """Fill this in"""
     assign_nodes = [n for n in node.body if isinstance(n, astroid.Assign)]
     call_nodes = [n.value for n in assign_nodes if isinstance(n.value, astroid.Call)]
 
     # Store nodes containing python_callable arg as:
     # {task_id: (call node, python_callable func name)}
-    python_callable_nodes = {}
+    task_ids_to_python_callable_specs = {}
     for call_node in call_nodes:
         if call_node.keywords:
             task_id = ""
@@ -40,9 +41,48 @@ def get_task_ids_to_python_callable_specs(
                     task_id = keyword.value.value
 
             if python_callable:
-                python_callable_nodes[task_id] = (call_node, python_callable)
+                task_ids_to_python_callable_specs[task_id] = (call_node, python_callable)
 
-    return python_callable_nodes
+    return task_ids_to_python_callable_specs
+
+
+def get_xcoms_from_tasks(
+    node: astroid.Module, task_ids_to_python_callable_specs: Dict[str, Tuple[astroid.Call, str]]
+) -> Tuple[Dict[str, Tuple[astroid.Call, str]], Set[str]]:
+    """Now fetch the functions mentioned by python_callable args"""
+    xcoms_pushed = {}
+    xcoms_pulled_taskids = set()
+    for (
+        task_id,
+        (python_callable, callable_func_name),
+    ) in task_ids_to_python_callable_specs.items():
+        if callable_func_name == "<lambda>":  # TODO support lambdas
+            continue
+
+        callable_func = node.getattr(callable_func_name)[0]
+
+        if not isinstance(callable_func, astroid.FunctionDef):
+            continue  # Callable_func is str not FunctionDef when imported
+
+        callable_func = node.getattr(callable_func_name)[0]
+
+        # Check if the function returns any values
+        if any(isinstance(n, astroid.Return) for n in callable_func.body):
+            # Found a return statement
+            xcoms_pushed[task_id] = (python_callable, callable_func_name)
+
+        # Check if the function pulls any XComs
+        callable_func_calls = callable_func.nodes_of_class(astroid.Call)
+        for callable_func_call in callable_func_calls:
+            if (
+                isinstance(callable_func_call.func, astroid.Attribute)
+                and callable_func_call.func.attrname == "xcom_pull"
+            ):
+                for keyword in callable_func_call.keywords:
+                    if keyword.arg == "task_ids":
+                        xcoms_pulled_taskids.add(keyword.value.value)
+
+    return xcoms_pushed, xcoms_pulled_taskids
 
 
 class XComChecker(checkers.BaseChecker):
@@ -57,37 +97,12 @@ class XComChecker(checkers.BaseChecker):
         XComs can be set (pushed) implicitly via return of a python_callable or
         execute() of an operator. And explicitly by calling xcom_push().
 
-        Currently this only checks unused XComs from return value of a python_callable.
+        Currently, this only checks unused XComs from return value of a python_callable.
         """
         python_callable_nodes = get_task_ids_to_python_callable_specs(node)
 
         # Now fetch the functions mentioned by python_callable args
-        xcoms_pushed = {}
-        xcoms_pulled_taskids = set()
-        for (task_id, (python_callable, callable_func_name)) in python_callable_nodes.items():
-            if callable_func_name != "<lambda>":
-                # TODO support lambdas
-                callable_func = node.getattr(callable_func_name)[0]
-
-                if isinstance(callable_func, astroid.FunctionDef):
-                    # Callable_func is str not FunctionDef when imported
-                    callable_func = node.getattr(callable_func_name)[0]
-
-                    # Check if the function returns any values
-                    if any(isinstance(n, astroid.Return) for n in callable_func.body):
-                        # Found a return statement
-                        xcoms_pushed[task_id] = (python_callable, callable_func_name)
-
-                    # Check if the function pulls any XComs
-                    callable_func_calls = callable_func.nodes_of_class(astroid.Call)
-                    for callable_func_call in callable_func_calls:
-                        if (
-                            isinstance(callable_func_call.func, astroid.Attribute)
-                            and callable_func_call.func.attrname == "xcom_pull"
-                        ):
-                            for keyword in callable_func_call.keywords:
-                                if keyword.arg == "task_ids":
-                                    xcoms_pulled_taskids.add(keyword.value.value)
+        xcoms_pushed, xcoms_pulled_taskids = get_xcoms_from_tasks(node, python_callable_nodes)
 
         self.check_unused_xcoms(xcoms_pushed, xcoms_pulled_taskids)
 
